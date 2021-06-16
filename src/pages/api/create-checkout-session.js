2eb77e1b1f348175d3136e7fe275f1af7682f427
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2021-06-16 12:46:25 +0300

    newwer version

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -4,36 +4,36 @@ export default async (req, res) => {
   //we apply destructuring on them
 
   const { email, items } = req.body;
-  const transformedItems = items.map(item => ({
+  const transformedItems = items.map((item) => ({
     price_data: {
-      currency: 'usd',
+      currency: "usd",
       unit_amount: item.price * 3600,
       description: item.description,
-      quabtity:1,
+      quantity: 1,
       product_data: {
-        name=item.title,
-        image=[item.image]
-      }
-    }
-  }))
+        name: item.title,
+        image: [item.image],
+      },
+    },
+  }));
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     shipping_rates: ["shr_1J2pGwFh0jPEdXAn5GiqYJSe"], // Created fees in Stripe's dashboard
     shipping_address_collection: {
-        allowed_countries: ["GB", "US", "CA", "FR"], // RTFM!
+      allowed_countries: ["GB", "US", "CA", "FR"], // RTFM!
     },
     line_items: transformedItems,
     mode: "payment",
     success_url: `${process.env.HOST}/success`,
     cancel_url: `${process.env.HOST}/checkout`,
     metadata: {
-        email,
-        images: JSON.stringify(groupedImages),
+      email,
+      images: JSON.stringify(groupedImages),
     },
-});
+  });
 
-console.log("session created!", session.id);
+  console.log("session created!", session.id);
 
-res.status(200).json({ id: session.id });
+  res.status(200).json({ id: session.id });
 };
